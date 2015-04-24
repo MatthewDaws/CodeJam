f56e424a4c69dd79279397f9d67f998fcac6f26d
MatthewDaws <matt.daws@cantab.net>
2015-04-24 21:53:01 +0100

    Update to correct version of c.py

--- 2015_1a/c.py
+++ 2015_1a/c.py
@@ -1,25 +1,35 @@
 # -*- coding: utf-8 -*-
 
-import math, fractions
+import math
 
 from collections import namedtuple
 Point = namedtuple("Point", ["x", "y"])
 
-atan2replace_magic = 4*(10**7)
-atan2replace_magic1 = 10**7
+def yield_removals(tree, trees):
+    """`tree` is current point of interest, and `trees` is an iterable
+    of all the other trees."""
+    angles = [ math.atan2(t.y - tree.y, t.x - tree.x) for t in trees ]
+    num_trees = len(angles)
+    angles.sort()
+    angles2 = [ x + 2.0*math.pi for x in angles ]
+    angles.extend(angles2)
+    startindex = 0
+    endindex = 0
+    while startindex < num_trees:
+        # Find smallest endindex with angles[endindex] >= angles[index] + math.pi
+        while endindex < len(angles) and angles[endindex] < angles[startindex] + math.pi - epsilon:
+            endindex += 1
+        # Don't want to count colinear points
+        realstartindex = startindex + 1
+        while angles[realstartindex] < angles[startindex] + epsilon:
+            realstartindex += 1
+        realstartindex -= 1
+        # Add in appropriate number to list
+        for _ in range(realstartindex - startindex + 1):
+           yield ( endindex - realstartindex - 1 )
+        # Next loop
+        startindex = realstartindex + 1
 
-def atan2replace(y, x):
-    if y >= 0:
-        if x >= 0:
-            return fractions.Fraction(y+1, x+1)
-        else:
-            return atan2replace_magic1 + fractions.Fraction(y+1, abs(x)+1)
-    else:
-        if x < 0:
-            return 2 * atan2replace_magic1 + fractions.Fraction(abs(y)+1, abs(x)+1)
-        else:
-            return 3 * atan2replace_magic1 + fractions.Fraction(abs(y)+1, abs(x)+1)
-    
 num_cases = int(input())
 
 for case in range(1, num_cases+1):
@@ -27,31 +37,16 @@ for case in range(1, num_cases+1):
     trees = [ [int(x) for x in input().split()] for _ in range(num_trees) ]
     trees = [ Point(line[0],line[1]) for line in trees]
 
+    # Worst angle is roughly atan(1e-6) approx 1e-8
+    epsilon = 1e-10
+
     output = []
     for index, tree in enumerate(trees):
         if num_trees - 1 <= 2:
             output.append(0)
-            continue
-        # angles from tree relative to horizontal x-axis
-        angles = [ atan2replace(t.y - tree.y, t.x - tree.x) for i, t in
-                    enumerate(trees) if index != i ]
-        print(tree, angles)
-        angles.sort()
-        #if case == 33:
-        #print(tree, angles)
-        #if case > 33: exit()
-        angles2 = [ x + atan2replace_magic for x in angles ]
-        angles.extend( angles2 )
-        startindex = 0
-        endindex = 0
-        lengths = []
-        while startindex < num_trees - 1:
-            # Find smallest endindex with angles[endindex] >= angles[startindex] + math.pi
-            while endindex < len(angles) and angles[endindex] < angles[startindex] + math.pi:
-                endindex += 1
-            lengths.append( endindex - startindex - 1 )
-            startindex += 1
-        output.append( min(lengths) )
+        else:
+            others = ( t for i, t in enumerate(trees) if i != index )
+            output.append( min(yield_removals(tree, others)) )
 
     print("Case #{}:".format(case))
     for x in output:
